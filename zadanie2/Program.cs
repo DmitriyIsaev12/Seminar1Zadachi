f6f115d8b47b63268472548dddb892669a5c1c6d
Dmitriy <dmitriyisaev2217736564@gmail.com>
2022-12-23 22:26:46 +0300

    Задача№2

---
+++ zadanie2/Program.cs
@@ -0,0 +1,8 @@
+﻿//Написать программу, которая на вход принимает 2 числа и выдает, какое из них больше
+int a = -9,b=-3;
+int max=a;
+if (a>b) max=a;
+if (b>max) max=b;
+
+Console.Write("max=");
+Console.WriteLine(max);
