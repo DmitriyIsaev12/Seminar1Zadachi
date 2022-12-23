76244087da0c3adcf4bce4dcf83c9543256e796a
Dmitriy <dmitriyisaev2217736564@gmail.com>
2022-12-23 22:34:46 +0300

    Задача№4

---
+++  zadanie4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Написать программу, которая на вход принимает 3 числа и выдает, какое из них больше
+int a = 44,b=100, c=78;
+int max=a;
+if (a>b) max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+
+Console.Write("max=");
+Console.WriteLine(max);
