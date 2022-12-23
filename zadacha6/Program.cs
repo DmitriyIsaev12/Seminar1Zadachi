770ee384847ccb59f82dec56f8dfba47ad321ec9
Dmitriy <dmitriyisaev2217736564@gmail.com>
2022-12-23 23:51:58 +0300

    Задача№6

---
+++ zadacha6/Program.cs
@@ -0,0 +1,12 @@
+﻿//Написать прогрмму, которая на вход принимает число и выдает, является ли число "четным" или "Нечетным" 
+// Четное - делится на 2 без остатка
+    int num1, rez1;
+  
+    Console.Write("Введите число : ");
+    num1= Convert.ToInt32(Console.ReadLine()); 
+    rez1 = num1 % 2;
+    if (rez1 == 0)
+ Console.WriteLine("{0} это четное число\n",num1);
+    else
+ Console.WriteLine("{0} это нечетное число\n",num1);
+
